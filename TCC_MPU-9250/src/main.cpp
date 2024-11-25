f4acc45ef4b22a56d6e82cd8c465294255817382
Lokopdis <146121761+Lokopdis@users.noreply.github.com>
2024-11-25 11:43:14 -0300

    ADD New Script

---
+++ TCC_MPU-9250/src/main.cpp
@@ -0,0 +1,36 @@
+/* Informações Gerais
+Código desenvolvido para realizar leituras de aceleração com o sensor MPU-9250
+
+O presente código conta com a configuração e preparo do sensor MPU-9250 sem a utilização
+de bibliotecas, realizando a leitura através de uma interrupção para garantir um taxa
+de amostragem fixa, além de demonstrar a configuração do barraemnto do I2C em velocidade
+máxima. Para esse código foi utilizado um microcontrolador ESP-32-DEVKIT-V1 (DOIT).
+
+Esse código foi desenvolvido para as atividas de TCC do curso de ENG. Controle e Automação
+IFSC - Chapecó 
+
+Desenvolvido por: Eduardo Turcatto
+Versão: NULL
+Data: 22/11/2024
+*/
+///////////////////////// BIBLIOTECAS /////////////////////////
+#include <Arduino.h>
+
+////////////////////////// VARIAVEIS //////////////////////////
+//----------------------------------------------------------//
+
+//////////////////// DECLARAÇÃO DE FUNÇÕES ///////////////////
+//----------------------------------------------------------//
+
+//////////////////////////// SETUP ///////////////////////////
+void setup() {
+  
+}
+
+/////////////////////////// LOOP ////////////////////////////
+void loop() {
+  
+}
+
+///////////////////////// FUNÇÕES ///////////////////////////
+//----------------------------------------------------------//
