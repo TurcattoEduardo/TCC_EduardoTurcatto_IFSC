586b6108a48fe7f16eea4882e282fbfb0129a59b
Lokopdis <146121761+Lokopdis@users.noreply.github.com>
2025-01-03 19:54:32 -0300

    la ele

--- Librarie/Serial_comm/Serial.py
+++ Librarie/Serial_comm/Serial.py
@@ -18,16 +18,7 @@ ID_ENC = 0xB2
 ID_CON = 0xC1
 
 # Função para calcular CRC-16 (polinômio 0xA001)
-def calculate_crc(data):
-    crc = 0xFFFF
-    for byte in data:
-        crc ^= byte
-        for _ in range(8):
-            if crc & 0x0001:
-                crc = (crc >> 1) ^ 0xA001
-            else:
-                crc >>= 1
-    return crc
+
 
 # Função para decodificar mensagens
 def decode_message(buffer):
